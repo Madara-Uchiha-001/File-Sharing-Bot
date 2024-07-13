29f7b9069bc31f109000b6bb32df95ed985562e9
Bookwormislive <infohubstore02@gmail.com>
2024-07-13 10:49:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "HELLO THERE {mention} ✌\n\nI AM A FILE SHARING BOT USED AND DEVELOPED BY  InfoHub Networks. 🍂\n\nI AM NOT DESIGNED TO RECEIVE DIRECT TEXT MESSAGES HERE. 🍂\n\nTHANKS FOR USING OUR SERVICES. 🍂")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "SORRY TO INTERRUPT YOU THERE, BUT TO CONTINUE FURTHER, YOU NEED TO JOIN OUR CHANNEL(S) FIRST 🫶")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
