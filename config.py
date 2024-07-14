5364999fa0fa654b7ef62a55e2606b39e2763e08
Bookworm2024 <infohubnetworks@gmail.com>
2024-07-14 09:11:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,36 +32,36 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002197933598"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002152311495"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "2"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "HELLO THERE {mention} ✌\n\nI AM A FILE SHARING BOT USED AND DEVELOPED BY  InfoHub Networks. 🍂\n\nI AM NOT DESIGNED TO RECEIVE DIRECT TEXT MESSAGES HERE. 🍂\n\nTHANKS FOR USING OUR SERVICES. 🍂")
+START_MSG = os.environ.get("START_MESSAGE", "Hello There! {mention}\n\nWelcome to My Premium Bot")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6011680723 1524169222").split()):
+    for x in (os.environ.get("ADMINS", "7144504368 5745818770").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "SORRY TO INTERRUPT YOU THERE, BUT TO CONTINUE FURTHER, YOU NEED TO JOIN OUR CHANNEL(S) FIRST 🫶")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Please verify yourself as human by joining the channel below 🤖⬇️")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "💀 WHY IN THE WORLD WILL YOU MESSAGE ME?? I'M JUST A FILE SHARE BOT WORKING FOR InfoHub Networks!!"
+USER_REPLY_TEXT = "💀 WHY IN THE WORLD WILL YOU MESSAGE ME?? I'M JUST A FILE SHARE BOT 🤫"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6011680723)
+ADMINS.append(7144504368)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
